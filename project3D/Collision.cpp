81ff175d3f90b2b02757d57ddfe4a28dc9f89d66
Ethan Moffatt <darker1300@gmail.com>
2017-06-21 15:10:06 +1000

    Static Collision restructuring.

--- project3D/Collision.cpp
+++ project3D/Collision.cpp
@@ -29,7 +29,7 @@ namespace Physics {
 
 		void Collision::ResolveCollision(Body * _obj1, Body * _obj2, CollisionInfo & _info)
 		{
-			if (_info.collided) {
+			if (_info.collided && !(_obj1->GetIsStatic() && _obj2->GetIsStatic())) {	// Collided and Not Both Static
 				HandleSeperation(_obj1, _obj2, _info);
 				HandleVelocityChange(_obj1, _obj2, _info);
 			}
@@ -37,40 +37,53 @@ namespace Physics {
 
 		void Collision::HandleSeperation(Body * _obj1, Body * _obj2, CollisionInfo & _info)
 		{
-			float totalMass = _obj1->GetMass() + _obj2->GetMass();
-			float obj1Offset = _info.penetration * (_obj2->GetMass() / totalMass);
-			float obj2Offset = _info.penetration * (_obj1->GetMass() / totalMass);
-
-			glm::vec3 obj1NewPos = _obj1->GetPosition() - (obj1Offset * _info.normal);
-			glm::vec3 obj2NewPos = _obj2->GetPosition() + (obj2Offset * _info.normal);
-
-			if (!_obj1->GetIsStatic())
+			if (_obj1->GetIsStatic() || _obj2->GetIsStatic()) {	// One Side is Dynamic
+				if (!_obj1->GetIsStatic()) {	// obj1 is Dynamic
+					glm::vec3 obj1NewPos = _obj1->GetPosition() - (_info.penetration * _info.normal);
+					_obj1->SetPosition(obj1NewPos);
+				}
+				else if (!_obj2->GetIsStatic()) {	// obj2 is Dynamic
+					glm::vec3 obj2NewPos = _obj2->GetPosition() + (_info.penetration * _info.normal);
+					_obj2->SetPosition(obj2NewPos);
+				}
+			}
+			else {	// Both Dynamic
+				float totalMass = _obj1->GetMass() + _obj2->GetMass();
+				float obj1Offset = _info.penetration * (_obj2->GetMass() / totalMass);
+				float obj2Offset = _info.penetration * (_obj1->GetMass() / totalMass);
+				glm::vec3 obj1NewPos = _obj1->GetPosition() - (obj1Offset * _info.normal);
+				glm::vec3 obj2NewPos = _obj2->GetPosition() + (obj2Offset * _info.normal);
 				_obj1->SetPosition(obj1NewPos);
-			if (!_obj2->GetIsStatic())
 				_obj2->SetPosition(obj2NewPos);
+			}
 		}
 
 		void Collision::HandleVelocityChange(Body * _obj1, Body * _obj2, CollisionInfo & _info)
 		{
+			float e;	// Restitution
+			if (_obj1->GetIsStatic() || _obj2->GetIsStatic()) {	// One Side is Dynamic
+				e = _obj1->GetIsStatic() ? _obj2->GetRestitution() : _obj1->GetRestitution();
+			}
+			else {	// Both Dynamic
+				e = (_obj1->GetRestitution() + _obj2->GetRestitution()) * 0.5f;	// Average Restitution
+			}
+
+			glm::vec3 relativeVelocity = _obj2->GetVelocity() - _obj1->GetVelocity();	// Relative Velocity
+
 			// Follows algorithm from http://chrishecker.com/Rigid_body_dynamics Subsection: Physics, Part 4: The Third Dimension. Page: 9/10.
-
-			// Average Restitution
-			float e = (_obj1->GetRestitution() + _obj2->GetRestitution()) * 0.5f;
-
-			glm::vec3 relativeVelocity = _obj2->GetVelocity() - _obj1->GetVelocity();
-
 			float jTop = -(1 + e) * glm::dot(relativeVelocity, _info.normal);
 			float jBottom = glm::dot(_info.normal, _info.normal) * (1.0f / _obj1->GetMass() + 1.0f / _obj2->GetMass());
-			// Impulse Magnitude
-			float j = jTop / jBottom;
+			float j = jTop / jBottom; // Impulse Magnitude
 
-			glm::vec3 obj1NewVelocity = _obj1->GetVelocity() - ((j / _obj1->GetMass()) * _info.normal);
-			glm::vec3 obj2NewVelocity = _obj2->GetVelocity() + ((j / _obj2->GetMass()) * _info.normal);
-
-			if (!_obj1->GetIsStatic())
+			// Apply resulting velocity to Dynamic objects
+			if (!_obj1->GetIsStatic()) {
+				glm::vec3 obj1NewVelocity = _obj1->GetVelocity() - ((j / _obj1->GetMass()) * _info.normal);
 				_obj1->SetVelocity(obj1NewVelocity);
-			if (!_obj2->GetIsStatic())
+			}
+			if (!_obj2->GetIsStatic()) {
+				glm::vec3 obj2NewVelocity = _obj2->GetVelocity() + ((j / _obj2->GetMass()) * _info.normal);
 				_obj2->SetVelocity(obj2NewVelocity);
+			}
 		}
 
 		namespace Test {
