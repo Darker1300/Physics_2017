8a6221af2276b972934c46cb3e63ada9a5a717e0
Ethan Moffatt <darker1300@gmail.com>
2017-06-26 17:29:12 +1000

    Added build, docs.

--- project3D/Sphere.cpp
+++
@@ -1,9 +0,0 @@
-#include "Sphere.h"
-
-#include "Body.h"
-#include <Gizmos.h>
-
-void Physics::Sphere::DrawGizmo(const Body * const _body, const glm::vec4 & _colour) const
-{
-	aie::Gizmos::addSphere(_body->GetPosition(), GetRadius(), 12, 12, _colour);
-}
